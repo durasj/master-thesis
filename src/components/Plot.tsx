e8e3b6cad8d8e2a1866f5188e2e118879a9e982b
Jakub Duras <jakub@duras.me>
2023-08-25 23:09:00 +0200

    Include first svg plots

--- src/components/Plot.tsx
+++ src/components/Plot.tsx
@@ -6,7 +6,13 @@ import type { Data, Layout, PlotlyHTMLElement } from 'plotly.js'
 /**
  * Hacky way to get plotly to work as next.js dynamic without SSR wouldn't work with react-plotly
  */
-export function Plot({ data, layout }: { data: Data[], layout: Partial<Layout> }) {
+export function Plot({
+  data,
+  layout,
+}: {
+  data: Data[]
+  layout: Partial<Layout>
+}) {
   const plotRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
@@ -19,7 +25,30 @@ export function Plot({ data, layout }: { data: Data[], layout: Partial<Layout> }
 
       plotRef.current.innerText = ''
 
-      await Plotly.newPlot(plotRef.current, data, layout, { autosizable: true, responsive: true })
+      await Plotly.newPlot(plotRef.current, data, layout, {
+        autosizable: true,
+        responsive: true,
+        displayModeBar: 'hover',
+        modeBarButtonsToAdd: [
+          {
+            name: 'toSvg',
+            gravity: 'f',
+            title: 'Download plot as a svg',
+            icon: Plotly.Icons.disk,
+            click: function (gd) {
+              Plotly.downloadImage(gd, {
+                format: 'svg',
+                filename:
+                  typeof layout.title === 'string'
+                    ? layout.title
+                    : layout.title?.text || 'plot',
+                width: 640,
+                height: layout.height || 450,
+              })
+            },
+          },
+        ],
+      })
     })()
   }, [data, layout])
 
